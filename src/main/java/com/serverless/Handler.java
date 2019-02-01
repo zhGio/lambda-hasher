8cec8d50af39a36e72daa70fe61652b22a3c09f4
Ognjen Mišić <ognjen.misic@codecentric.de>
2019-02-01 10:32:48 +0100

    get rid of this from local machine

---
+++ src/main/java/com/serverless/Handler.java
@@ -0,0 +1,23 @@
+package com.serverless;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<String, String> {
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	public String getSaltedHashedPassword(String password) {
+		String salt = BCrypt.gensalt(12);
+		return BCrypt.hashpw(password, salt);
+	}
+
+	@Override
+	public String handleRequest(String input, Context context) {
+		return getSaltedHashedPassword(input);
+	}
+}
